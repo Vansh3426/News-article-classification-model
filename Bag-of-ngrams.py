ff6e2f019c01f23b5d1d7f6357895021011bd88c
Vansh Mhatre <vanshmhatre342006@gmail.com>
2025-12-27 23:05:47 +0530

    Model created

---
+++ Bag-of-ngrams.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+import numpy as np 
+import sklearn 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import classification_report
+from sklearn.compose import ColumnTransformer
+import spacy
+
+
+
+
+df = pd.read_json("News-article-classification/news_dataset.json")
+
+
+
+train = df["text"]
+target = df["category"]
+
+
+Xtrain ,Xtest , ytrain ,ytest  = train_test_split(train,target , test_size=0.2)
+
+
+pipe =Pipeline([("vectorizer",CountVectorizer(ngram_range=(1,2))),
+                ("model", MultinomialNB())])
+
+model = pipe.fit(Xtrain , ytrain)
+
+pred = pipe.predict(Xtest)
+
+print(classification_report(ytest,pred))
+
+# raw_text =[
+#     "Elon musk buys twitter for 42 billion$",
+#     "sunil chettri turns the match to his side by scoring 3 goals at second half",
+#     "light takes 8 min to reach earth form the sun",
+#     "A theft broke the glass and perform day time robbery in washington"
+# ]
+# pred =model.predict(raw_text)
+
+# print(pred)
